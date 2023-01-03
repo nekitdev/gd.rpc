cd97ba368ecda8107bd870b6ea4b3a99819c53bf
Nikita Tikhonov <nekitdevofficial@gmail.com>
2023-01-03 07:00:57 +0300

    Prepare the release.

--- gd/rpc/__init__.py
+++ gd/rpc/__init__.py
@@ -6,7 +6,7 @@ __url__ = "https://github.com/nekitdev/gd.rpc"
 __title__ = "rpc"
 __author__ = "nekitdev"
 __license__ = "MIT"
-__version__ = "1.0.0-alpha.1"
+__version__ = "1.0.0"
 
 from gd.rpc.config import CONFIG_NAME, DEFAULT_CONFIG, GD_NAME, NAME, Config, ConfigData, get_config
 from gd.rpc.main import rpc
